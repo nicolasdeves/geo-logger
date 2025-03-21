c3214ea0ef2953c43e05b24ed2a71737a05bc7c3
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-03-20 23:02:50 -0300

    feat: add main logic and sqlite connection

--- app/src/main/java/com/nicolas/geo_logger/DatabaseManager.java
+++ app/src/main/java/com/nicolas/geo_logger/DatabaseManager.java
@@ -1,34 +1,87 @@
 package com.nicolas.geo_logger;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseManager extends SQLiteOpenHelper {
 
-    private String createTableSQL = null;
-    private String dropTableSQL = null;
+    private static final String DATABASE_NAME = "geo_logger_db";
+    private static final int DATABASE_VERSION = 1;
 
-    public DatabaseManager(Context context, String dataBaseName, int version, String createTableSQL, String dropTableSQL) {
-        super(context, dataBaseName, null, version);
+    private static final String CREATE_TABLE_SQL = "CREATE TABLE location (" +
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                            "latitude REAL, " +
+                            "longitude REAL, " +
+                            "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
 
-        this.createTableSQL = createTableSQL;
-        this.dropTableSQL = dropTableSQL;
+    private static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS location;";
+
+    public DatabaseManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public DatabaseManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public void insertLocation(double latitude, double longitude) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sql = "INSERT INTO location (latitude, longitude) VALUES (?, ?)";
+        db.execSQL(sql, new Object[]{latitude, longitude});
+        db.close();
+    }
+
+    public int getQuantity() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "SELECT COUNT(*) FROM location";
+        int count = 0;
+
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.moveToFirst()) {
+            count = cursor.getInt(0);
+        }
+        cursor.close();
+        db.close();
+
+        return count;
+    }
+
+    public List<LatLng> getLastHourRoute() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "SELECT latitude, longitude FROM location WHERE timestamp >= DATETIME('now', '-1 hour') ORDER BY timestamp DESC";
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<LatLng> route = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+            do {
+                double latitude = cursor.getDouble(cursor.getColumnIndexOrThrow("latitude"));
+                double longitude = cursor.getDouble(cursor.getColumnIndexOrThrow("longitude"));
+                route.add(new LatLng(latitude, longitude));
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        return route;
+    }
+
+    public void cleanData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE from location");
+        db.close();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(createTableSQL);
-
+        db.execSQL(CREATE_TABLE_SQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(dropTableSQL);
+        db.execSQL(DROP_TABLE_SQL);
         onCreate(db);
     }
 }
--- app/src/main/java/com/nicolas/geo_logger/MainActivity.java
+++ app/src/main/java/com/nicolas/geo_logger/MainActivity.java
@@ -1,38 +1,55 @@
 package com.nicolas.geo_logger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    private TextView latitudeTv, longitudeTv = null;
-    private Button viewPositionsButton = null;
+    private DatabaseManager databaseManager;
+
+    private TextView latitudeTv, longitudeTv, countTv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponents();
+        databaseManager = new DatabaseManager(this);
     }
 
     public void initComponents() {
         latitudeTv = findViewById(R.id.tv_latitude);
         longitudeTv = findViewById(R.id.tv_longitude);
-        viewPositionsButton = findViewById(R.id.button_view_positions);
+        countTv = findViewById(R.id.tv_count);
+
+        Button viewPositionsButton = findViewById(R.id.button_view_positions);
+        viewPositionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button cleanDataButton = findViewById(R.id.button_clean);
+        cleanDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseManager.cleanData();
+            }
+        });
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -43,9 +60,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        latitudeTv.setText(String.valueOf(location.getLatitude()));
-        longitudeTv.setText(String.valueOf(location.getLongitude()));
+        latitudeTv.setText(String.format("%.5f", location.getLatitude()));
+        longitudeTv.setText(String.format("%.5f", location.getLongitude()));
+        countTv.setText(String.valueOf(databaseManager.getQuantity()));
 
-        //salvar no banco depois
+        databaseManager.insertLocation(location.getLatitude(), location.getLongitude());
     }
+
+    public void openMap(View view) {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 }
